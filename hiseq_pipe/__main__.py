559f632434029f27c3fdf36cefd3d9011db4ea9e
audy <harekrishna@gmail.com>
2013-12-30 14:28:06 -0500

    print help

--- hiseq_pipe/__main__.py
+++ hiseq_pipe/__main__.py
@@ -2,16 +2,31 @@ import sys
 
 import demultiplexer
 
+
 def main():
 
-    command = sys.argv[1]
-    del sys.argv[1] # remove command name so argparse works.
 
-    if command == 'help':
-        print 'coming soon!'
-    elif command == 'demultiplex':
-        demultiplexer.main()
+    commands = {
+            'demultiplex': demultiplexer.main,
+            }
 
+    try:
+        command = sys.argv[1]
+        del sys.argv[1]
+    except IndexError:
+        command = None
+
+
+    if command not in commands:
+        print 'unknown command: %s' % command
+        print 'available commands:\n'
+        for command in commands:
+            print command
+        quit(-1)
+    else:
+        command_function = commands[command]
+
+    command_function()
 
 if __name__ == '__main__':
     main()
