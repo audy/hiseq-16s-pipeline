12207c648992132f45aeb1b807f14715618549fc
Austin Davis-Richardson <harekrishna@gmail.com>
2014-08-16 11:20:41 -0400

    simplified

--- qsubs/illumina-split.sh
+++ qsubs/illumina-split.sh
@@ -9,34 +9,33 @@
 #
 # REQUIRED ENVIRONMENT VARIABLES
 #
-# - EXPERIMENT   - name of the experiment
-# - QUAL_TYPE    - type of quality scores
-# - BARCODES     - location of barcodes file
 # - LEFT_READS   - location of left reads
 # - BC_READS     - location of barcode reads
 # - RIGHT_READS  - location of right reads
+# - BARCODES     - location of barcodes file
+# - EXPERIMENT   - prefix for output and sample id
+#
+
+#
+# OPTIONAL ENVIRONMENT VARIABLES
+# - QUAL_TYPE    - type of quality scores (default: sanger)
 #
 
 #
 # EXAMPLE
 #
-# qsub -v EXPERIMENT='my-experiment' \
-#      -v BARCODES='my-barcodes.csv' \
+# qsub -v BARCODES='my-barcodes.csv' \
 #      -v LEFT_READS='left-reads.fastq.gz' \
 #      -v RIGHT_READS='right-reads.fastq.gz' \
 #      -v BC_READS='bc-reads.fastq.gz'
 
-# locate of scratch directory
-SCRATCH='/scratch/lfs/adavisr'
 
 set -e
 set -x
 
 date
 
-cd $SCRATCH
-
-mkdir -p ${EXPERIMENT}-samples
+cd $PBS_O_WORKDIR
 
 hp-label-by-barcode \
   --barcodes $BARCODES \
@@ -45,10 +44,10 @@ hp-label-by-barcode \
   --left-reads ${LEFT_READS} \
   --right-reads ${RIGHT_READS} \
   --barcode-reads ${BC_READS} \
+  --bc-seq-proc 'lambda b: b[0:7]' \
   --output-format fastq \
   --gzip \
   --id-format "${EXPERIMENT}.B.%(sample_id)s %(index)s orig_bc=%(bc_seq)s new_bc=%(bc_seq)s bc_diffs=0" \
-  --bc-seq-proc 'lambda b: b[0:7]' \
   --output /dev/stdout |
 hp-split-by-barcode \
   --output-directory ${EXPERIMENT}-samples
