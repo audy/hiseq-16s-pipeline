f98b0cbe4e24749c89530388fdab26d8bbeda5f7
Austin Davis-Richardson <harekrishna@gmail.com>
2014-10-20 11:02:32 -0400

    barcode length (default=12)

--- qsubs/illumina-split.sh
+++ qsubs/illumina-split.sh
@@ -18,12 +18,11 @@
 
 #
 # EXAMPLE
-#
-# qsub -v BARCODES='my-barcodes.csv' \
-#      -v LEFT_READS='left-reads.fastq.gz' \
-#      -v RIGHT_READS='right-reads.fastq.gz' \
-#      -v BC_READS='bc-reads.fastq.gz' \
-#      -v EXPERIMENT='poop-experiment'
+# (with required arguments)
+
+# qsub -v BARCODES='my-barcodes.csv',LEFT_READS='left-reads.fastq.gz',RIGHT_READS='right-reads.fastq.gz',BC_READS='bc-reads.fastq.gz',EXPERIMENT='poop-experiment'
+
+# optional: BARCODE_LENGTH=7 # specify barcode length, default is 12 (miseq + golay barcodes), use 7 for triplett/miseq barcodes
 
 
 set -e
@@ -44,7 +43,7 @@ hp-label-by-barcode \
   --left-reads ${LEFT_READS} \
   --right-reads ${RIGHT_READS} \
   --barcode-reads ${BC_READS} \
-  --bc-seq-proc 'lambda b: b[0:7]' \
+  --bc-seq-proc 'lambda b: b[0:${BARCODE_LENGTH:=12}]' \
   --output-format fastq \
   --gzip \
   --id-format "${EXPERIMENT}.B.%(sample_id)s %(index)s orig_bc=%(bc_seq)s new_bc=%(bc_seq)s bc_diffs=0" \
